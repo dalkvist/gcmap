93651adc9c491040f9c5ea528add807599d4c8bb
Mattias Dalkvist <mattias@nividev.com>
2013-03-01 16:49:36 +0100

    clojure version

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject gcmap "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [cheshire "2.0.6"]
                  [clj-http "0.2.7"]
                  [noir "1.2.2"]
